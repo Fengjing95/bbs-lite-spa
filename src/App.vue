e8f7f882e0919dee7a46710a3202904788cb05af
Fengjing95 <1984779164@qq.com>
2019-09-19 21:58:41 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+  <!--路由管道标签，任何符合某一路由(route)信息的组件都会在这个标签内展示出来 -->
+    <router-link v-bind:to="'/'">Home</router-link>
+    <router-link v-bind:to="'/about'">About</router-link>
+    <router-link v-bind:to="'/param'">Param Link</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<!-- css格式 -->
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
